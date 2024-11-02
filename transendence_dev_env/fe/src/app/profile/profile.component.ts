5a3b0658cda215c6cc4fffb313245b60e3a886b4
LukeDerpwalka <lukaskreuzer@outlook.de>
2024-11-03 00:52:06 +0100

    UI improvements

--- transendence_dev_env/fe/src/app/profile/profile.component.ts
+++ transendence_dev_env/fe/src/app/profile/profile.component.ts
@@ -7,6 +7,7 @@ import { Game, GameService } from '../games/game.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
   profileForm!: FormGroup;
