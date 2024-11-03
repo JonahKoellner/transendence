0a1c005643dcb65e7c6a2babe45d9ed6c24e69c9
LukeDerpwalka <lukaskreuzer@outlook.de>
2024-11-03 01:41:10 +0100

    UI improvements

--- transendence_dev_env/fe/src/app/home/home/home.component.ts
+++ transendence_dev_env/fe/src/app/home/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth.service';
+import { Game } from 'src/app/games/game.service';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +10,37 @@ import { AuthService } from 'src/app/auth.service';
 })
 export class HomeComponent {
 
+  recentGames: Game[] = [];
+
   constructor(private authService: AuthService, private router: Router) {}
 
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+  navigateToMode(mode: string) {
+    switch (mode) {
+      case 'pve':
+        this.router.navigate(['/games/local-pve']);
+        break;
+      case 'local_pvp':
+        this.router.navigate(['/games/local-pvp']);
+        break;
+      case 'online_pvp':
+        this.router.navigate(['/games/online-pvp']);
+        break;
+      case 'tournament': 
+        this.router.navigate(['/games/tournament/local/start']);
+        break;
+      case 'arena':
+        this.router.navigate(['/games']);
+        break;
+    }
+  }
+  navigateToLeaderboards() {
+    this.router.navigate(['/leaderboards']);
+  }
+  navigateToGameSelection() {
+    this.router.navigate(['/games']);
   }
 }
