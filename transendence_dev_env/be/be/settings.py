581c2653e6320a2c93a35e83975496a65072dbfe
Jonah Köllner <jonahkollner@MacBookPro.fritz.box>
2024-11-20 20:36:08 +0100

    Added ssl certificated

--- transendence_dev_env/be/be/settings.py
+++ transendence_dev_env/be/be/settings.py
@@ -176,7 +176,8 @@ if not DEBUG:
     # Security settings for production only
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
-    SECURE_SSL_REDIRECT = False
+    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SECURE_HSTS_SECONDS = 31536000  # One year
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
