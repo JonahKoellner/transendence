f2fd524331c98db30929399c44933aaf276c6a42
LukeDerpwalka <lukaskreuzer@outlook.de>
2024-11-21 17:26:42 +0100

    Add PDF viewer to About component with responsive styling

--- transendence_dev_env/fe/src/app/home/about/about.component.ts
+++ transendence_dev_env/fe/src/app/home/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
+  pdfSrc: string = 'assets/subject.pdf';
+
+  constructor(public sanitizer: DomSanitizer) { }
 
 }
