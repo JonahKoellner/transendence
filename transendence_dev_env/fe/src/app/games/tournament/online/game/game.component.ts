759a3d3b1259f38d96bdf1e20cb80d854efae8ac
nburchha <niklas@burchhardt.de>
2025-01-09 12:45:06 +0100

    decided to reuse the game-display component from onlinepvp, removed canvas and game drawing methods from game component

--- transendence_dev_env/fe/src/app/games/tournament/online/game/game.component.ts
+++ transendence_dev_env/fe/src/app/games/tournament/online/game/game.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild, EventEmitter, Output, Input, HostListener } from '@angular/core';
+import { OnInit, OnDestroy, Component, ElementRef, ViewChild, EventEmitter, Output, Input, HostListener } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { GameDisplayService } from 'src/app/services/game-display.service';
@@ -22,13 +22,11 @@ interface GameSettings {
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent implements AfterViewInit {
+export class GameComponent implements OnInit, OnDestroy {
   @Output() gameEnd = new EventEmitter<void>();
-  @ViewChild('gameCanvas') canvas!: ElementRef<HTMLCanvasElement>;
   @Input() matchId: string = '';
   @Input() userProfile: UserProfile | null = null;
   private roomId: string = '';
-  context!: CanvasRenderingContext2D;
   private messageSubscription!: Subscription;
 
   //for everything around the canvas
@@ -38,23 +36,9 @@ export class GameComponent implements AfterViewInit {
   leftScore: number = 0;
   rightScore: number = 0;
 
-  gameState: any = null;
   //TODO make this right, currently use default values
-  gameSettings: GameSettings = {
-    paddleskin_color_left: 'white',
-    paddleskin_color_right: 'white',
-    ballskin_color: 'white',
-    gamebackground_color: 'black',
-  };
-
-  // Image elements
-  private paddleImageLeft: HTMLImageElement | null = null;
-  private paddleImageRight: HTMLImageElement | null = null;
-  private ballImage: HTMLImageElement | null = null;
-  private backgroundImage: HTMLImageElement | null = null;
-
-  // Flags to check if images are loaded
-  private imagesLoaded: boolean = false;
+  gameState: any = null;
+  gameSettings: GameSettings = {};
 
   constructor(
     private toastr: ToastrService,
@@ -74,7 +58,7 @@ export class GameComponent implements AfterViewInit {
         console.log('Received message:', msg);
       }
       if (msg.type === 'game_state') {
-        this.handleGameState(msg.data);
+        this.handleGameState(msg);
       } else if (msg.type === 'game_ended') {
         this.handleGameEnd(msg.winner);
       } else if (msg.type === 'game_started') {
@@ -86,21 +70,6 @@ export class GameComponent implements AfterViewInit {
     });
   }
 
-  ngAfterViewInit() {
-    if (!this.canvas) {
-      console.error('Canvas not found');
-      return;
-    }
-    const context = this.canvas.nativeElement.getContext('2d');
-    if (context) {
-      this.context = context;
-      // if (this.imagesLoaded) {
-      // this.drawGame();
-    // } else {
-    //   console.warn('Images not loaded yet, Game will be drawn when images are loaded');
-    }
-  }
-
   ngOnDestroy(): void {
     if (this.messageSubscription) {
       this.messageSubscription.unsubscribe();
@@ -108,16 +77,14 @@ export class GameComponent implements AfterViewInit {
     this.gameDisplayService.disconnect();
   }
 
-  private handleGameState(state: any): void {
+  private handleGameState(msg: any): void {
+    let state = msg.state
     if (this.leftScore !== state.left_score || this.rightScore !== state.right_score) {
       console.log('Score updated:', state.left_score, state.right_score);
     }
     this.leftScore = state.left_score;
     this.rightScore = state.right_score;
-    this.gameState = state;
-    if (this.context) {
-      this.drawGame();
-    }
+    this.gameState = msg;
   }
 
   private handleGameEnd(winner: any) {
@@ -166,159 +133,5 @@ export class GameComponent implements AfterViewInit {
       action: 'keyup',
       key: event.code,
     });
-  }
-
-  /**
-   * Loads images based on the game settings.
-   * Returns a promise that resolves when all images are loaded or skipped if not provided.
-   */
-  private loadImages(): Promise<void> {
-    const promises: Promise<void>[] = [];
-
-    if (this.gameSettings.paddleskin_image_left) {
-      promises.push(this.loadImage(this.gameSettings.paddleskin_image_left, (img) => (this.paddleImageLeft = img)));
-    } else {
-      this.paddleImageLeft = null;
-    }
-
-    if (this.gameSettings.paddleskin_image_right) {
-      promises.push(this.loadImage(this.gameSettings.paddleskin_image_right, (img) => (this.paddleImageRight = img)));
-    } else {
-      this.paddleImageRight = null;
-    }
-
-    if (this.gameSettings.ballskin_image) {
-      promises.push(this.loadImage(this.gameSettings.ballskin_image, (img) => (this.ballImage = img)));
-    } else {
-      this.ballImage = null;
-    }
-
-    if (this.gameSettings.gamebackground_wallpaper) {
-      promises.push(this.loadImage(this.gameSettings.gamebackground_wallpaper, (img) => (this.backgroundImage = img)));
-    } else {
-      this.backgroundImage = null;
-    }
-
-    return Promise.all(promises)
-      .then(() => {
-        this.imagesLoaded = true;
-      })
-      .catch((err) => {
-        this.imagesLoaded = false;
-        throw err;
-      });
-  }
-
-  /**
-   * Helper function to load an image.
-   * @param src Image source URL
-   * @param assignFn Callback to assign the loaded image
-   */
-  private loadImage(src: string, assignFn: (img: HTMLImageElement) => void): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-      const img = new Image();
-      img.src = src;
-      img.onload = () => {
-        assignFn(img);
-        resolve();
-      };
-      img.onerror = () => {
-        this.toastr.warning(`Failed to load image from ${src}`, 'Warning');
-        resolve(); // Resolve even on error to avoid blocking
-      };
-    });
-  }
-
-  /**
-   * Draws the entire game state on the canvas.
-   */
-  drawGame() {
-    // Draw background
-    this.drawBackground();
-
-    // Draw ball
-    this.drawBall(this.gameState.ball_x, this.gameState.ball_y);
-
-    // Draw left paddle
-    this.drawPaddle(0, this.gameState.left_paddle_y, 'left');
-
-    // Draw right paddle
-    this.drawPaddle(990, this.gameState.right_paddle_y, 'right');
-  }
-
-  /**
-   * Draws the game background using an image or color.
-   */
-  private drawBackground() {
-    if (this.backgroundImage) {
-      // Draw the background image stretched to canvas size
-      this.context.drawImage(this.backgroundImage, 0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
-    } else if (this.gameSettings.gamebackground_color) {
-      // Fill with background color
-      this.context.fillStyle = this.gameSettings.gamebackground_color;
-      this.context.fillRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
-    } else {
-      // Default background
-      this.context.fillStyle = 'black';
-      this.context.fillRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
-    }
-  }
-
-  /**
-   * Draws the ball using an image or color.
-   * @param x X-coordinate of the ball
-   * @param y Y-coordinate of the ball
-   */
-  drawBall(x: number, y: number) {
-    if (this.ballImage && this.imagesLoaded) {
-      const ballSize = 30; // Diameter of the ball
-      this.context.drawImage(this.ballImage, x - ballSize / 2, y - ballSize / 2, ballSize, ballSize);
-    } else if (this.gameSettings.ballskin_color) {
-      this.context.beginPath();
-      this.context.arc(x, y, 15, 0, 2 * Math.PI);
-      this.context.fillStyle = this.gameSettings.ballskin_color;
-      this.context.fill();
-    } else {
-      // Default ball color
-      this.context.beginPath();
-      this.context.arc(x, y, 15, 0, 2 * Math.PI);
-      this.context.fillStyle = 'white';
-      this.context.fill();
-    }
-  }
-
-  /**
-   * Draws a paddle using an image or color.
-   * @param x X-coordinate of the paddle
-   * @param y Y-coordinate of the paddle
-   * @param side 'left' or 'right' to determine which paddle to draw
-   */
-  drawPaddle(x: number, y: number, side: 'left' | 'right') {
-    const paddleWidth = 10;
-    const paddleHeight = 60;
-
-    // console.log("Draw Paddle: " + (this.paddleImageLeft != null) + " " + this.imagesLoaded);
-    if (side === 'left') {
-      if (this.paddleImageLeft) {
-        this.context.drawImage(this.paddleImageLeft, x, y, paddleWidth, paddleHeight);
-        return;
-      }
-    } else if (side === 'right') {
-      if (this.paddleImageRight) {
-        this.context.drawImage(this.paddleImageRight, x, y, paddleWidth, paddleHeight);
-        return;
-      }
-    }
-
-    // If no image, use color
-    if (side === 'left' && this.gameSettings.paddleskin_color_left) {
-      this.context.fillStyle = this.gameSettings.paddleskin_color_left;
-    } else if (side === 'right' && this.gameSettings.paddleskin_color_right) {
-      this.context.fillStyle = this.gameSettings.paddleskin_color_right;
-    } else {
-      this.context.fillStyle = 'white'; // Default paddle color
-    }
-
-    this.context.fillRect(x, y, paddleWidth, paddleHeight);
   }
 }
