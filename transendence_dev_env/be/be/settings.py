173e219cb46b95f437b3586677571a63d9e9d6c9
Jonah Koellner <jonah@koellner.com>
2025-01-17 08:52:21 +0100

    CHanged Django secret key from static to generate .env

--- transendence_dev_env/be/be/settings.py
+++ transendence_dev_env/be/be/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-j=&_)ux49j@6hdnbf6dunf^vpq(8*40eke4(%&2!6ru3l3iu(@"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
